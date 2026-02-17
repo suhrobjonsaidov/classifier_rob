592f84803f45e1965cb6fc42b980cf0d85a22b43
Suhrobjon Saidov <suhrobjonsaidov@tensorai.io>
2026-02-16 18:23:31 -0800

    succesfully turnedd the texts into vectors through embedding

---
+++ classifier/embed.py
@@ -0,0 +1,21 @@
+"""
+we load the csv file
+load the sentence model
+turn each text row into a vector using embedding
+and then we save the embeddings
+"""
+#imports
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+
+
+#loading the model for the embedding
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+df = pd.read_csv("file.csv")
+
+texts = df["text"].to_list()
+
+embeddings = model.encode(texts)
+
+print(embeddings.shape)
