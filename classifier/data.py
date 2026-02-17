5330c9068015a0e7cbf4251c6017b9e217824aab
Suhrobjon Saidov <rob@Suhrobjons-MBP.localdomain>
2026-02-16 17:19:18 -0800

    used a class and a loop, reduce the code size and clean it up

--- classifier/data.py
+++ classifier/data.py
@@ -11,15 +11,25 @@ include only fields that we need for embedding
 and then save it as a csv with pandas
 """
 
-math = load_dataset("gsm8k", "main") 
-# print(ds["train"][0])
-# print(math["train"][0]['question'])
+# data loading from hggf
+datasets_config = {
+    "WRITING": {"dataset": "aeslc", "field": "email_body"},
+    "MATH": {"dataset": "gsm8k", "field": "question"},
+    "KNOWLEDGE": {"dataset": "trivia_qa", "field": "question"},
+    "DATA_ANALYSIS": {"dataset": "datadrivenscience/datasets-for-analysis", "field": "Description"},
+    "TRANSLATION": {"dataset": "opus_books", "field": "translation"},
+    "CODING": {"dataset": "codeparrot/apps", "field": "question"},
+    "LEGAL": {"dataset": "lex_glue", "field": "context"},
+    "SUMMARIZATION": {"dataset": "cnn_dailymail", "field": "article"}
+}
 
 q_list = []
 
-for i in range(200):
-    row = math["train"][i]
-    q_list.append({"category" : "MATH", "text": row["question"]})
+for category, config in datasets_config.items():
+    for i in range(200):
+        row = config["dataset"]["train"][i]
+        q_list.append({"category": category, "text": row[config["field"]]})
+
 
 df = pd.DataFrame(q_list)
 
